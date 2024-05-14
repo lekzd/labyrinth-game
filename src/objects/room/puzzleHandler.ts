cf3737f0bf8c2ce173ab95168e7b92e899281b29
Aleksandr Korotaev <lekzd@mail.ru>
2024-05-15 01:23:57 +0300

    feat: add puzzle stone texture

--- src/objects/room/puzzleHandler.ts
+++ src/objects/room/puzzleHandler.ts
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import * as TWEEN from '@tweenjs/tween.js'
 import { MapObject } from "../../types/MapObject";
 import { systems } from "../../systems";
+import { loads } from "../../loader";
 
 export const createPuzzleHandler = () => {
   const target = new THREE.Object3D()
@@ -14,13 +15,42 @@ export const createPuzzleHandler = () => {
     ),
     new THREE.MeshPhongMaterial({ color: 0xff0000 }),
   )
+  const runicTexture = loads.texture["runic_2.png"]?.clone()!
+  runicTexture.wrapS = THREE.RepeatWrapping
+  runicTexture.wrapT = THREE.RepeatWrapping
+
+  runicTexture.repeat = new THREE.Vector2(1 / 4, 1 / 3)
+
+  const materials: THREE.MeshLambertMaterial[] = []
+
+  const coordsMap = [
+    { x: 1, y: 0 },
+    { x: 0, y: 1 },
+    { x: 1, y: 1 },
+    { x: 1, y: 2 },
+    { x: 2, y: 1 },
+    { x: 3, y: 2 },
+  ]
+
+  for (let i = 0; i < 6; i++) {
+    const map = loads.texture["runic_2.png"]?.clone()!
+    const { x, y } = coordsMap[i]
+  
+    map.repeat = new THREE.Vector2(1 / 4, 1 / 3)
+    map.offset = new THREE.Vector2((1 / 4) * x, (1 / 3) * y)
+
+    materials.push(
+      new THREE.MeshLambertMaterial({ map,  })
+    )
+  }
+
   const cube = new THREE.Mesh(
     new THREE.BoxGeometry(
       6,
       6,
       6,
     ),
-    new THREE.MeshPhongMaterial({ color: 0xffcc00 }),
+    materials,
   )
 
   cube.castShadow = true
