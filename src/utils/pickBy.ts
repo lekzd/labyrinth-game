5c61a09fde2403f41e7c3a54f21e54bbdeb5b804
Aleksandr Korotaev <lekzd@mail.ru>
2024-05-20 02:33:11 +0300

    fix: add typings for pickBy method

--- src/utils/pickBy.ts
+++ src/utils/pickBy.ts
@@ -1,12 +1,13 @@
-export const pickBy = <T>(obj, keys) => {
-  const next = {};
 
-  if (!obj) return next;
+export const pickBy = <T extends {}, K extends keyof T>(obj: T, keys: K[]): Pick<T, K> => {
+  const next: Partial<T> = {};
+
+  if (!obj) return next as Pick<T, K>;
 
   for (const key of keys) {
     if (key in obj)
       next[key] = obj[key];
   }
 
-  return next as T;
+  return next as Pick<T, K>;
 }
