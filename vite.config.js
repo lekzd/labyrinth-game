b2833309f899d9c866874a4293360444067afe93
Alexey <lifeprojob@gmail.com>
2024-05-20 19:18:07 +0300

    add aliases

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import glsl from 'vite-plugin-glsl';
-import { defineConfig } from 'vite';
+import glsl from "vite-plugin-glsl";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [glsl()]
+  plugins: [glsl()],
+  resolve: {
+    alias: {
+      "@": "/src/",
+      "@utils": "/src/utils/",
+      "@uses": "/src/uses/",
+      "@types": "/src/types/",
+    },
+  },
 });
