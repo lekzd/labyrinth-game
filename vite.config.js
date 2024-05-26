902cebf89912ce1cf9d5fdc2c25de18330a51c7a
Aleksandr Korotaev <lekzd@mail.ru>
2024-05-26 22:12:09 +0300

    feat: add react plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import glsl from "vite-plugin-glsl";
+import react from '@vitejs/plugin-react'
 import { defineConfig } from "vite";
+import path from 'path'
 
 export default defineConfig({
-  plugins: [glsl()],
+  plugins: [react(), glsl()],
   resolve: {
     alias: {
-      "@": "/src/",
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
