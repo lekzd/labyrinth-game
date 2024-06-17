568c678d76aca386016fd5d117772a073538293e
Aleksandr Korotaev <lekzd@mail.ru>
2024-06-17 17:27:49 +0400

    fix: jittering using sin noize

--- src/utils/jitterGeometry.ts
+++ src/utils/jitterGeometry.ts
@@ -1,5 +1,4 @@
 import { BufferGeometry } from "three";
-import { frandom } from "./random";
 
 export const jitterGeometry = (geometry: BufferGeometry, amount: number): BufferGeometry => {
   const positionAttribute = geometry.getAttribute('position');
@@ -12,9 +11,12 @@ export const jitterGeometry = (geometry: BufferGeometry, amount: number): Buffer
     let z = positionAttribute.getZ(i);
 
     // Сдвигаем вершину в случайных направлениях
-    x += frandom(-amount, amount);
-    y += frandom(-amount, amount);
-    z += frandom(-amount, amount);
+    // x += frandom(-amount, amount);
+    // y += frandom(-amount, amount);
+    // z += frandom(-amount, amount);
+    x += Math.sin(x) * amount;
+    y += Math.sin(y) * amount;
+    z += Math.sin(z) * amount;
 
     // Устанавливаем новые координаты вершины
     positionAttribute.setXYZ(i, x, y, z);
