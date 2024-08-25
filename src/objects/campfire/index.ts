f360aeb0a34c835beb2125c4bbdcde13b9d6263e
Aleksandr Korotaev <lekzd@mail.ru>
2024-08-25 23:42:22 +0300

    feat: campfire healing effect

--- src/objects/campfire/index.ts
+++ src/objects/campfire/index.ts
@@ -1,6 +1,7 @@
 import {
   BufferAttribute,
   BufferGeometry,
+  Color,
   Mesh,
   MeshBasicMaterial,
   Object3D,
@@ -8,7 +9,7 @@ import {
   PointLight,
   Points,
   ShaderMaterial,
-  SphereGeometry,
+  SphereGeometry
 } from "three";
 
 import { frandom } from "@/utils/random";
@@ -16,27 +17,89 @@ import vertexShader from "./shader.vert";
 import fragmentShader from "./shader.frag";
 import { assign } from "@/utils/assign";
 import { DynamicObject } from "@/types";
+import { state } from "@/state";
+import { getDistance } from "@/utils/getDistance";
+import { settings } from "../hero/settings";
+import { throttle } from "@/utils/throttle.ts";
+
+const healHealth = throttle((object: DynamicObject) => {
+  state.setState({
+    objects: {
+      [object.id]: { health: object.health + 1 }
+    }
+  });
+}, 500);
 
 const PARTICLE_COUNT = 100;
 export class Campfire {
   readonly props: DynamicObject;
   readonly mesh: Object3D<Object3DEventMap>;
   private particleMaterial: ShaderMaterial;
+  private torch: PointLight;
+
   constructor(props: DynamicObject) {
+    this.healing = false;
     this.props = props;
     this.particleMaterial = new ShaderMaterial({
       uniforms: {
         time: { value: 0.0 },
         size: { value: 0.1 },
+        healing: { value: 0.0 }
       },
       vertexShader,
-      fragmentShader,
+      fragmentShader
     });
-    this.mesh = initMesh(props, this.particleMaterial);
+
+    const { base, torch } = initMesh(props, this.particleMaterial);
+
+    this.torch = torch;
+    this.mesh = base;
   }
+
+  onStateChange(prev, next) {
+    if (!next) return;
+
+    if (next.hasOwnProperty('state')) {
+      this.torch.color = new Color(next.state ? 0x00ff33 : 0xff4500);
+      this.props.state = next.state;
+
+      this.particleMaterial.uniforms.healing.value = +next.state;
+      this.particleMaterial.uniformsNeedUpdate = true;
+    }
+  }
+
   update(timeDelta: number) {
+    let healing = false;
+
+    Object.entries(state.objects).forEach(([id, object]) => {
+      if (id === this.props.id) {
+        return;
+      }
+
+      if (
+        !["Monk", "Cleric", "Rogue", "Warrior", "Wizard"].includes(object?.type)
+      ) {
+        return;
+      }
+
+      const distance = getDistance(this.props.position, object.position);
+
+      if (distance < 30 && object.health < settings[object.type].health) {
+        healHealth(object);
+        healing = true;
+      }
+    });
+
     this.particleMaterial.uniforms.time.value += timeDelta * 2;
     this.particleMaterial.uniformsNeedUpdate = true;
+
+    if (this.props.state !== healing) {
+      state.setState({
+        objects: {
+          [this.props.id]: { state: healing }
+        }
+      });
+    }
   }
 }
 
@@ -85,5 +148,5 @@ function initMesh(props: DynamicObject, particleMaterial: ShaderMaterial) {
   base.add(torch);
 
   assign(base.position, props.position);
-  return base;
+  return { base, torch };
 }
