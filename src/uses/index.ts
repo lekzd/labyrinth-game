2bd1f129317a13d99fd1dcb8dd0d0523b8ae5a18
snipeek <snipeek>
2025-01-04 20:09:26 +0300

    add ci

--- src/uses/index.ts
+++ src/uses/index.ts
@@ -1,7 +1,7 @@
-export { Box } from "@/objects/box";
-export { Hero } from "@/objects/hero";
-export { Torch } from "@/objects/torch";
-export { Campfire } from "@/objects/campfire";
-export { Weapon } from "@/objects/weapon";
-export { PuzzleHandler } from "@/objects/puzzleHandler";
+export { Box } from "@/objects/box/index.ts";
+export { Hero } from "@/objects/hero/index.ts";
+export { Torch } from "@/objects/torch/index.ts";
+export { Campfire } from "@/objects/campfire/index.ts";
+export { Weapon } from "@/objects/weapon/index.ts";
+export { PuzzleHandler } from "@/objects/puzzleHandler/index.ts";
 export { Room } from "@/objects/room/Room";
