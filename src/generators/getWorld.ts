2d8f5c6f1746b6339a579b4fc62f9f320fafe424
Aleksandr Korotaev <lekzd@mail.ru>
2025-03-18 22:03:37 +0300

    fix: return back campfire

--- src/generators/getWorld.ts
+++ src/generators/getWorld.ts
@@ -10,7 +10,7 @@ export const getWorld = (x: number, y: number, n = 25, k = 0.7) => {
 
   const dungeons = noise(x / (n * k * 0.035), y / (n * k * 0.035));
 
-  if (x === 0 && y === 0) return Tiles.MagicMushroom;
+  if (x === 0 && y === 0) return Tiles.Campfire;
 
   if (Math.sin(ground * 15) < -0.6) return Tiles.Road;
 
